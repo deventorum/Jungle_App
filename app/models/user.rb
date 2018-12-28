65acd12328f0478e64dc95436d70956a94f22283
deventorum <deventorum@gmail.com>
2018-12-28 01:17:35 +0000

    Database table users is fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  validates :email, presence: true, uniqueness: true
+  class User < ActiveRecord::Base
+    has_secure_password
+    validates :email, presence: true, uniqueness: true
+  end
 end
